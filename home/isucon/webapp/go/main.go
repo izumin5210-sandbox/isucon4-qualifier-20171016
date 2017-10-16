d1d908ce1eb8fa1851c37166f7a260549d5991ab
izumin5210 <masayuki@izumin.info>
2017-10-17 00:34:00 +0900

    Use gin instead of martini

--- home/isucon/webapp/go/main.go
+++ home/isucon/webapp/go/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"github.com/go-martini/martini"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/martini-contrib/render"
-	"github.com/martini-contrib/sessions"
 	"net/http"
 	"strconv"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
@@ -46,19 +46,19 @@ func init() {
 }
 
 func main() {
-	m := martini.Classic()
+	r := gin.Default()
 
 	store := sessions.NewCookieStore([]byte("secret-isucon"))
-	m.Use(sessions.Sessions("isucon_go_session", store))
+	r.Use(sessions.Sessions("isucon_go_session", store))
 
-	m.Use(render.Renderer())
-
-	m.Get("/", func(r render.Render, session sessions.Session) {
-		r.HTML(200, "index", map[string]string{"Flash": getFlash(session, "notice")})
+	r.GET("/", func(c *gin.Context) {
+		session := sessions.Default(c)
+		c.HTML(200, "index", map[string]string{"Flash": getFlash(session, "notice")})
 	})
 
-	m.Post("/login", func(req *http.Request, r render.Render, session sessions.Session) {
-		user, err := attemptLogin(req)
+	r.POST("/login", func(c *gin.Context) {
+		session := sessions.Default(c)
+		user, err := attemptLogin(c.Request)
 
 		notice := ""
 		if err != nil || user == nil {
@@ -72,33 +72,34 @@ func main() {
 			}
 
 			session.Set("notice", notice)
-			r.Redirect("/")
+			c.Redirect(302, "/")
 			return
 		}
 
 		session.Set("user_id", strconv.Itoa(user.ID))
-		r.Redirect("/mypage")
+		c.Redirect(302, "/mypage")
 	})
 
-	m.Get("/mypage", func(r render.Render, session sessions.Session) {
+	r.GET("/mypage", func(c *gin.Context) {
+		session := sessions.Default(c)
 		currentUser := getCurrentUser(session.Get("user_id"))
 
 		if currentUser == nil {
 			session.Set("notice", "You must be logged in")
-			r.Redirect("/")
+			c.Redirect(302, "/")
 			return
 		}
 
 		currentUser.getLastLogin()
-		r.HTML(200, "mypage", currentUser)
+		c.HTML(200, "mypage", currentUser)
 	})
 
-	m.Get("/report", func(r render.Render) {
-		r.JSON(200, map[string][]string{
+	r.GET("/report", func(c *gin.Context) {
+		c.JSON(200, map[string][]string{
 			"banned_ips":   bannedIPs(),
 			"locked_users": lockedUsers(),
 		})
 	})
 
-	http.ListenAndServe(":8080", m)
+	http.ListenAndServe(":8080", r)
 }
